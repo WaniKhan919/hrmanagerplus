825c58a2988eb4f8f768af0938539979a2dc9eb0
Wanikhan919 <wanikhan919@gmail.com>
2023-07-25 00:43:03 +0500

    all done

--- mail.php
+++ mail.php
@@ -1,29 +1,33 @@
 <?php
-$id=$_GET['uuid'];
-function readHtmlFile($filePath) {
-    return file_get_contents($filePath);
+// Function to fetch the content of a URL using curl
+function fetchUrlContent($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    return $content;
 }
 
+// Function to send email with HTML content
 function sendEmailWithHtmlContent($to, $subject, $htmlContent) {
-
-    $headers = "From: wanikhan919@gmail.com" . "\r\n";
-    $headers .= "Reply-To: wanikhan919@gmail.com" . "\r\n";
+    // Set email headers
+    $headers = "From: your_email@example.com" . "\r\n";
+    $headers .= "Reply-To: your_email@example.com" . "\r\n";
     $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
 
-
+    // Send the email
     mail($to, $subject, $htmlContent, $headers);
 }
 
-$htmlFilePath = 'confirmation-modify-printview.php?uuid='.$id;
-$htmlContent = readHtmlFile($htmlFilePath);
+// Example usage:
+$confirmationPageUrl = 'https://example.com/confirmation-modify-printview.php?uuid=72b84a86-5c8a-494f-a717-3b9078a607b9';
+$htmlContent = fetchUrlContent($confirmationPageUrl);
 
-
-$to = 'wanikhan919@gmail.com';
+// Replace these values with appropriate email and subject
+$to = 'recipient@example.com';
 $subject = 'Your Subject';
 
-
-$mail=sendEmailWithHtmlContent($to, $subject, $htmlContent);
-if($mail){
-    header('location:success.php');
-}
+// Send the email with the HTML content
+sendEmailWithHtmlContent($to, $subject, $htmlContent);
 ?>
